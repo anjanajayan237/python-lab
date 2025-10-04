ba5365b16ca9c7e017c2eeec574eb72e5a43060e
anjanajayan237 <anjanajayan237@gmail.com>
2025-10-04 11:08:07 +0530

    Add files via upload

---
+++ large_3.py
@@ -0,0 +1,9 @@
+n1= int(input("Enter first number"))
+n2= int(input("Enter second number"))
+n3= int(input("Enter third number"))
+if n1>n2 and n1>n3:
+    print("largest number is",n1)
+elif n2>n3 and n2>n1:
+    print("largest no is",n2)
+else:
+    print("largest no is",n3)
