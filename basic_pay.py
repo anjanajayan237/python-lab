8616317d3cc5f77f3fbad98d831c2bfca084adf3
anjanajayan237 <anjanajayan237@gmail.com>
2025-10-04 10:53:43 +0530

    Add files via upload

---
+++ basic_pay.py
@@ -0,0 +1,8 @@
+basic_pay=float(input("Enter your basic pay"))
+hra=0.10*basic_pay
+ta=0.05*basic_pay
+salary=basic_pay+hra+ta
+print("basic pay:",basic_pay)
+print("hra :",hra)
+print("ta:",ta)
+print("salary of employee:",salary)
