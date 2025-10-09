ff8f9ef029bd6984cf5aeffc4b5ee131d604a22d
anjanajayan237 <anjanajayan237@gmail.com>
2025-10-09 14:48:43 +0530

    Add files via upload

---
+++ dictionaryc.py
@@ -0,0 +1,17 @@
+# Program to maintain student records using a dictionary
+# Keys - Student names
+# Values - List of marks in 3 subjects
+students = {
+ "Anu": [85, 90, 78],
+ "Gowri": [72, 88, 91],
+ "Vishnu": [95, 80, 85]
+}
+# Display student records
+for name, marks in students.items():
+ total = sum(marks) # total marks
+ average = sum(marks) / len(marks) # average marks
+ print(f"Student: {name}")
+ print(f"Marks: {marks}")
+ print(f"Total Marks: {total}")
+ print(f"Average Marks: {average:.2f}")
+ print("." * 20)
